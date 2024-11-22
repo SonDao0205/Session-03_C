93e45bfdeeb0ab33be60b10f838611464865bc4b
sondt0205 <daotruongson2006@gmail.com>
2024-11-22 08:25:54 +0700

    Update Session-03_Ex-07.c

--- Session-03_Ex-07.c
+++ Session-03_Ex-07.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main(){
-    int number, temp, sum = 0;
-    printf("Nhap so nguyen co 4 chu so : ");
+    int number, num1, num2, num3, num4, sum;
+    printf("Nhập một số nguyên có 4 chữ số: ");
     scanf("%d", &number);
-    temp = number;
-    while (temp > 0)
-    {
-        sum += temp % 10;
-        temp /= 10;
-    }
+    num1 = number % 10;
+    num2 = (number / 10) % 10;
+    num3 = (number / 100) % 10;
+    num4 = number / 1000;
+    sum = num1 + num2 + num3 + num4;
     printf("Tong cua 4 so nguyen = %d", sum);
     return 0;
 }
